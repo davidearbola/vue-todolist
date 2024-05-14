e6edc3511fe6600d2af96a4d2fd55a850d2df75f
davidearbola <davidearbola@gmail.com>
2024-05-14 15:49:32 +0200

    Bonus 1 - invece di gestire un array esegue le stesse funzioni ma gestendo un array di oggetti

--- js/main.js
+++ js/main.js
@@ -5,12 +5,36 @@ const { createApp } = Vue;
 createApp({
 	data() {
 		return {
+			// 		toDoList: [
+			// 			"Milestone 0",
+			// 			"Milestone 1",
+			// 			"Milestone 2",
+			// 			"Bonus",
+			// 			"Bonus 2",
+			// 		],
+			// 		valoreInput: "",
+			// 		removeIcon: "fa-solid fa-x",
+			// 	};
+			// },
+			// methods: {
+			// 	addToDo(valore, array) {
+			// 		console.log(valore);
+			// 		array.push(valore);
+			// 		console.log(array);
+			// 		this.valoreInput = ""; // valore = ""; non funziona
+			// 	},
+			// 	removeToDo(array, indice) {
+			// 		console.log(array[indice]);
+			// 		array.splice(indice, 1);
+			// 		console.log(array);
+			// 	},
+			// },
 			toDoList: [
-				"Milestone 0",
-				"Milestone 1",
-				"Milestone 2",
-				"Bonus",
-				"Bonus 2",
+				{ stringa: "Milestone 0", done: true },
+				{ stringa: "Milestone 1", done: true },
+				{ stringa: "Milestone 2", done: true },
+				{ stringa: "Bonus", done: true },
+				{ stringa: "Bonus 2", done: true },
 			],
 			valoreInput: "",
 			removeIcon: "fa-solid fa-x",
@@ -18,15 +42,11 @@ createApp({
 	},
 	methods: {
 		addToDo(valore, array) {
-			console.log(valore);
-			array.push(valore);
-			console.log(array);
+			array.push({ stringa: valore, done: true });
 			this.valoreInput = ""; // valore = ""; non funziona
 		},
 		removeToDo(array, indice) {
-			console.log(array[indice]);
 			array.splice(indice, 1);
-			console.log(array);
 		},
 	},
 	mounted() {
